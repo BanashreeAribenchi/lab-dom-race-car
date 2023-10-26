bd3eab9c5aa66a3a5f3d823ffd0b6d196b3d5bed
Banashree Aribenchi <banashree.aribenchi17@gmail.com>
2023-10-26 18:19:28 +0200

    7 steps done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  // let game;
+  game = new Game();
 
   startButton.addEventListener("click", function () {
     startGame();
@@ -8,5 +10,49 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+    // game = new Game();
+
+    game.start();
   }
+
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeysstrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+    if (possibleKeysstrokes.includes(key)) {
+      event.preventDefault();
+
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          console.log("Hello arrowleft");
+          break;
+
+        case "ArrowUp":
+          game.player.directionY = -1;
+          console.log("Hello ArrowUp");
+
+          break;
+
+        case "ArrowRight":
+          game.player.directionX = 1;
+          console.log("Hello ArrowRight");
+
+          break;
+
+        case "ArrowDown":
+          game.player.directionY = 1;
+          console.log("Hello ArrowDown");
+
+          break;
+      }
+    }
+  }
+
+  window.addEventListener("keydown", handleKeydown);
+  // window.addEventListener("keyup", handleKeyup);
 };
